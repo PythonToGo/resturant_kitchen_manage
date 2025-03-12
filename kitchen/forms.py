472ff9be38fadf704322f0f130dc8842c3010509
PythonToGo <pythontogoplease@gmail.com>
2025-03-13 00:53:52 +0100

    feat: add views, forms and urls

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -1,8 +1,72 @@
 from django import forms
-from .models import Dish
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+from kitchen.models import Dish, Cook, DishType
 
 
-class DishForm(forms.ModelForm):
+class DishTypeForm(forms.ModelForm):
+    dishes = forms.ModelMultipleChoiceField(
+        queryset=Dish.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
     class Meta:
         model = Dish
         fields = "__all__"
+
+
+class CookForm(forms.Form):
+    cook = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    
+    class Meta:
+        model = Cook
+        fields = ("name", "description", "price", "dish_type", "cook")
+
+
+class CookCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience",
+        )
+
+
+class CookUpdateForm(forms.ModelForm):
+    dishes = forms.ModelMultipleChoiceField(
+        queryset=Dish.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ("first_name", "last_name", "years_of_experience", "dishes")
+
+
+class DishForm(forms.ModelForm):
+    cook = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+    
+    class Meta:
+        model = Dish
+        fields = ("name", "description", "price", "dish_type", "cook")
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by Name"}),
+    )
